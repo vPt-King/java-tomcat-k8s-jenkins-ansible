d6e4a66786b8171fbeb149d16d99211bae214edd
Thanh <vpthanh1302@gmail.com>
2024-07-09 16:28:11 +0700

    oke ansible

--- bash.sh
+++ bash.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sed -i "s|image: thanhvu638/javaweb:v[^\"]*|image: thanhvu638/javaweb:v$1|g" ./app.yaml
 name_deployment=$(kubectl get deployment | awk '{print $1}' | grep tomcat)
 kubectl delete deployment $name_deployment 
 kubectl apply -f app.yaml
